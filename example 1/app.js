403f78a90cc7b2a00c97b87f75da54f93666f70e
ThaiChinh1905 <thaichinh1905@gmail.com>
2024-04-09 09:09:57 +0700

    remove comment server

--- example 1/app.js
+++ example 1/app.js
@@ -73,7 +73,6 @@ const createWorker = async () => {
   console.log(`worker pid ${worker.pid}`)
 
   worker.on('died', error => {
-    // This implies something serious happened, so kill the application
     console.error('mediasoup worker has died')
     setTimeout(() => process.exit(1), 2000) // exit in 2 seconds
   })
@@ -81,13 +80,8 @@ const createWorker = async () => {
   return worker
 }
 
-// We create a Worker as soon as our application starts
 worker = createWorker()
 
-// This is an Array of RtpCapabilities
-// https://mediasoup.org/documentation/v3/mediasoup/rtp-parameters-and-capabilities/#RtpCodecCapability
-// list of media codecs supported by mediasoup ...
-// https://github.com/versatica/mediasoup/blob/v3/src/supportedRtpCapabilities.ts
 const mediaCodecs = [
   {
     kind: 'audio',
@@ -113,17 +107,11 @@ peers.on('connection', async socket => {
   })
 
   socket.on('disconnect', () => {
-    // do some cleanup
     console.log('peer disconnected')
   })
 
   socket.on('createRoom', async (callback) => {
     if (router === undefined) {
-      // worker.createRouter(options)
-      // options = { mediaCodecs, appData }
-      // mediaCodecs -> defined above
-      // appData -> custom application data - we are not supplying any
-      // none of the two are required
       router = await worker.createRouter({ mediaCodecs, })
       console.log(`Router ID: ${router.id}`)
     }
@@ -136,12 +124,8 @@ peers.on('connection', async socket => {
     callback({ rtpCapabilities })
   }
 
-  // Client emits a request to create server side Transport
-  // We need to differentiate between the producer and consumer transports
   socket.on('createWebRtcTransport', async ({ sender }, callback) => {
     console.log(`Is this a sender request? ${sender}`)
-    // The client indicates if it is a producer or a consumer
-    // if sender is true, indicates a producer else a consumer
     if (sender) {
       producerSocketId = socket.id
       producerTransport = await createWebRtcTransport(callback)
@@ -152,15 +136,12 @@ peers.on('connection', async socket => {
     }
   })
 
-  // see client's socket.emit('transport-connect', ...)
   socket.on('transport-connect', async ({ dtlsParameters }) => {
     console.log('DTLS PARAMS... ', { dtlsParameters })
     await producerTransport.connect({ dtlsParameters })
   })
 
-  // see client's socket.emit('transport-produce', ...)
   socket.on('transport-produce', async ({ kind, rtpParameters, appData }, callback) => {
-    // call produce based on the prameters from the client
     producer = await producerTransport.produce({
       kind,
       rtpParameters,
@@ -173,22 +154,18 @@ peers.on('connection', async socket => {
       producer.close()
     })
 
-    // Send back to the client the Producer's id
     callback({
       id: producer.id
     })
   })
 
-  // see client's socket.emit('transport-recv-connect', ...)
   socket.on('transport-recv-connect', async ({ dtlsParameters }) => {
     console.log(`DTLS PARAMS: ${dtlsParameters}`)
     await consumerTransport.connect({ dtlsParameters })
   })
 
   socket.on('consume', async ({ rtpCapabilities }, callback) => {
-    // console.log('produce id',producer.id, "rtp cap",rtpCapabilities);
     try {
-      // check if the router can consume the specified producer
       if (router.canConsume({
         producerId: producer.id,
         rtpCapabilities
@@ -196,7 +173,6 @@ peers.on('connection', async socket => {
 
         console.log('can consume');
         
-        // transport can now consume and return a consumer
         consumer = await consumerTransport.consume({
           producerId: producer.id,
           rtpCapabilities,
@@ -212,8 +188,6 @@ peers.on('connection', async socket => {
           console.log('producer of consumer closed')
         })
 
-        // from the consumer extract the following params
-        // to send back to the Client
         const params = {
           id: consumer.id,
           producerId: producer.id,
@@ -221,7 +195,6 @@ peers.on('connection', async socket => {
           rtpParameters: consumer.rtpParameters,
         }
 
-        // send the parameters to the client
         callback({ params })
       }
     } catch (error) {
@@ -281,7 +254,6 @@ const createWebRtcTransport = async (callback) => {
       console.log('transport closed')
     })
 
-    // send back to the client the following prameters
     callback({
       // https://mediasoup.org/documentation/v3/mediasoup-client/api/#TransportOptions
       params: {
